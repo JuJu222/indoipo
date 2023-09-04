6dd6bfa7bc8c4fa0f01de22a0b459ea290ad2ff0
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2023-09-04 21:26:11 +0700

    t

---
+++ src/components/animations/Reveal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+function Reveal({ children }) {
+    return (
+        <>
+            <motion.div>
+
+            </motion.div>
+        </>
+    );
+}
+
+export default Reveal;
