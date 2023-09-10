72103a62e88b7ca671e8c2cdd39f52f074581bd5
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2023-09-10 17:29:23 +0700

    t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,43 @@
 import './globals.css'
-import type { Metadata } from 'next'
-import { Quicksand } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Quicksand} from 'next/font/google'
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Head from 'next/head'
+import Script from 'next/script'
 
-const quicksand = Quicksand({ subsets: ['latin'] })
+const quicksand = Quicksand({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'Indoipo - Analisa IPO dengan Mudah',
-  description: 'Analisa IPO dengan Mudah',
+    title: 'Indoipo - Analisa IPO dengan Mudah',
+    description: 'Analisa IPO dengan Mudah',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <script async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6258132678019384"
-                crossOrigin="anonymous"></script>
-      </head>
-      <body className={quicksand.className + ' bg-white'}>
-        <Navbar></Navbar>
-          <main>
-            {children}
-          </main>
-        <Footer></Footer>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-FQQYZWNC68" />
+            <Script id="google-analytics">
+                {`
+                          window.dataLayer = window.dataLayer || [];
+                          function gtag(){dataLayer.push(arguments);}
+                          gtag('js', new Date());
+                 
+                          gtag('config', 'G-FQQYZWNC68');
+                        `}
+            </Script>
+            <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6258132678019384" />
+            <body className={quicksand.className + ' bg-white'}>
+                <Navbar></Navbar>
+                <main>
+                    {children}
+                </main>
+                <Footer></Footer>
+            </body>
+        </html>
+    )
 }
