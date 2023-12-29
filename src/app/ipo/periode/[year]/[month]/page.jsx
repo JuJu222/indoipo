d32be5f7c9ba8cc3ffb4a6d110320d2674a88a64
JuJu222 <japjustin@yahoo.com>
2023-12-30 02:00:33 +0700

    Moved period page to correct location

---
+++ src/app/ipo/periode/[year]/[month]/page.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import prisma from "@/lib/prisma";
+import {notFound} from "next/navigation";
+import Reveal from "src/components/animations/Reveal";
+import CompanyCard from "src/components/CompanyCard";
+
+export const metadata = {
+    title: 'Semua IPO | Indoipo - Analisa IPO dengan Mudah',
+    description: 'Dapatkan informasi yang mudah dimengerti mengenai IPO terbaru. Lihat PER, PBV, DER, ROE, dan metrik-metrik lainnya untuk membantu anda dalam menentukan investasi anda. Indoipo membantu anda dalam menyediakan metrik-metrik serta informasi keuangan dari perusahaan yang sedang IPO dalam bentuk yang sederhana.'
+}
+
+export const revalidate = 60
+
+async function Period(props) {
+    var firstDate = new Date(Date.UTC(props.params.year, parseInt(props.params.month) - 1, 1));
+    var lastDate = new Date(Date.UTC(props.params.year, parseInt(props.params.month), 0));
+
+    const companies = await prisma.company.findMany({
+        where: {
+            financials: { some: {} },
+            date_awal_start: {
+                lte: lastDate,
+                gte: firstDate,
+            },
+        },
+        orderBy: {
+            date_awal_start: 'desc'
+        },
+        include: {
+            subsector: {
+                include: {
+                    sector: true
+                }
+            },
+            financials: {
+                orderBy:  {
+                    date_end: 'desc'
+                }
+            }
+        }
+    });
+
+    if (companies.length === 0) {
+        return notFound()
+    }
+
+    return (
+        <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+                <h1 className='text-center text-4xl font-bold'>Semua IPO</h1>
+                {/*<Reveal>*/}
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
+                    {companies.map((company, index) => (
+                        <CompanyCard key={index} company={company}></CompanyCard>
+                    ))}
+                </div>
+                {/*</Reveal>*/}
+            </div>
+        </section>
+    )
+}
+
+export default Period;
