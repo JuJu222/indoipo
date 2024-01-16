27b41a10ed4d6679aced47d82526a3276edcac5a
JuJu222 <japjustin@yahoo.com>
2024-01-16 14:34:26 +0700

    Updated period page title

--- src/app/ipo/periode/[year]/[month]/page.jsx
+++ src/app/ipo/periode/[year]/[month]/page.jsx
@@ -8,7 +8,7 @@ export async function generateMetadata({ params }) {
     let firstDate = new Date(Date.UTC(params.year, parseInt(params.month) - 1, 1));
 
     return {
-        title: `IPO ${firstDate.toLocaleString('id-ID', { month: 'short' })} ${firstDate.getFullYear()} | Indoipo - Analisa IPO dengan Mudah`,
+        title: `Daftar IPO ${firstDate.toLocaleString('id-ID', { month: 'long' })} ${firstDate.getFullYear()} | Indoipo - Analisa IPO dengan Mudah`,
         description: `Daftar perusahaan IPO selama periode ${firstDate.toLocaleString('id-ID', { month: 'long' })} ${firstDate.getFullYear()}. Lihat PER, PBV, DER, ROE, dan metrik-metrik lainnya untuk membantu anda dalam menentukan investasi anda. Indoipo membantu anda dalam menyediakan metrik-metrik serta informasi keuangan dari perusahaan yang sedang IPO dalam bentuk yang sederhana.`
     }
 }
