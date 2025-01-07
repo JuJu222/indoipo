dea74a1dd6469af8eed109efe94eb6b19137217e
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2025-01-07 22:20:10 +0700

    Fixed images

--- next.config.js
+++ next.config.js
@@ -1,45 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'juju.sgp1.cdn.digitaloceanspaces.com',
-                port: '',
-                pathname: '/indoipo/img/**',
-            },
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'japard-public.s3.ap-southeast-1.amazonaws.com',
+        port: '',
+        pathname: '/indoipo.com/img/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://indoipo.com/:path*',
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
         ],
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://indoipo.com/:path*',
-            },
-        ]
-    },
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-                    },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value:
-                            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-                    },
-                ],
-            },
-        ];
-    },
-}
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
